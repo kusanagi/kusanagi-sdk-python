e447bd40feb84d10cea2e9d98f8663a45eb9c66a
James Watts <contact@jameswatts.info>
2020-01-25 19:33:50 +0100

    Update language support

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
     ],
     classifiers=[
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Operating System :: POSIX :: Linux',
